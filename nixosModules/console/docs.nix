5e90629bcf351aed5b4bbab8c4e662ad5143d686
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-12-09 10:02:27 +0100

    fix(nixosModules): :bug: fix docs module

--- nixosModules/console/docs.nix
+++ nixosModules/console/docs.nix
@@ -22,10 +22,10 @@
   ...
 }:
 let
-  inherit (lib.options) mkOption;
+  inherit (lib.options) mkOption mkEnableOption;
   inherit (lib.lists) flatten optionals;
   inherit (lib.types) bool;
-  inherit (lib.modules) mkIf mkDefault mkEnableOption;
+  inherit (lib.modules) mkIf mkDefault;
 
   cfg = config.biapy.nixos-unified.nixos.console.docs;
 
@@ -53,51 +53,51 @@ in
 
       full-doc = mkEnableOption "full documentation (NixOS, packages, and developers)";
     };
-
-    config = mkIf cfg.enable {
-      environment.defaultPackages =
-        with pkgs;
-        flatten [
-          (optionals cfg.man-pages)
-          [
-            man-pages
-            man-pages-posix
-          ]
-          (optionals cfg.cheat-sheats)
-          [
-            cht-sh # command-line interface for the Cheat.sh service
-            tealdeer # `tldr` A very fast implementation of tldr in Rust
-          ]
-        ];
-
-      documentation = {
-        # install documentation of packages from environment.systemPackages into the generated system path.
-        enable = mkDefault true;
-
-        # install info pages and the info command. This also includes "info" outputs.
-        info.enable = mkDefault true;
-
-        # install manual pages.
-        man = {
-          enable = mkDefault cfg.man-pages.enable; # enable manual pages and the man command.
-
-          # generate the manual page index caches using mandb(8).
-          # This allows searching for a page or keyword using utilities like apropos(1).
-          generateCaches = mkDefault true;
-
-          man-db.enable = mkDefault true;
-        };
-
-        # install documentation targeted at developers.
-        dev.enable = mkDefault cfg.full-docs;
-
-        # install documentation distributed in packages' /share/doc.
-        doc.enable = mkDefault cfg.full-docs;
-
-        # install NixOS's own documentation.
-        nixos.enable = mkDefault cfg.full-docs;
-      };
-    };
-
   };
+
+  config = mkIf cfg.enable {
+    environment.defaultPackages =
+      with pkgs;
+      flatten [
+        (optionals cfg.man-pages)
+        [
+          man-pages
+          man-pages-posix
+        ]
+        (optionals cfg.cheat-sheats)
+        [
+          cht-sh # command-line interface for the Cheat.sh service
+          tealdeer # `tldr` A very fast implementation of tldr in Rust
+        ]
+      ];
+
+    documentation = {
+      # install documentation of packages from environment.systemPackages into the generated system path.
+      enable = mkDefault true;
+
+      # install info pages and the info command. This also includes "info" outputs.
+      info.enable = mkDefault true;
+
+      # install manual pages.
+      man = {
+        enable = mkDefault cfg.man-pages.enable; # enable manual pages and the man command.
+
+        # generate the manual page index caches using mandb(8).
+        # This allows searching for a page or keyword using utilities like apropos(1).
+        generateCaches = mkDefault true;
+
+        man-db.enable = mkDefault true;
+      };
+
+      # install documentation targeted at developers.
+      dev.enable = mkDefault cfg.full-docs;
+
+      # install documentation distributed in packages' /share/doc.
+      doc.enable = mkDefault cfg.full-docs;
+
+      # install NixOS's own documentation.
+      nixos.enable = mkDefault cfg.full-docs;
+    };
+  };
+
 }
