7b201674965a7584dcc7bcaebdc434a1380cf404
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-12-10 05:38:07 +0100

    feat(nixosModules): :sparkles: add nix sane defaults module

---
+++ nixosModules/system/nix-sane-defaults.nix
@@ -0,0 +1,33 @@
+/**
+  Automatically run the garbage collector at a specific time.
+
+  @see https://search.nixos.org/options?channel=unstable&query=nix.gc
+*/
+{ config, lib, ... }:
+let
+  inherit (lib.options) mkEnableOption;
+  inherit (lib.modules) mkIf mkDefault;
+
+  cfg = config.biapy.nixos-unified.nixos.system.nix-sane-defaults;
+in
+{
+  options = {
+    biapy.nixos-unified.nixos.system.nix-sane-defaults.enable = mkEnableOption "ZSA keyboards";
+  };
+
+  config = mkIf cfg.enable {
+    nix = {
+      gc = {
+        automatic = mkDefault true;
+        options = mkDefault "--delete-older-than 7d";
+      };
+      settings = {
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+        auto-optimise-store = mkDefault true;
+      };
+    };
+  };
+}
