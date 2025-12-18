7b0010b310e9f9fa9d1307b3a697e3d01d7c0247
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-12-18 11:25:44 +0100

    docs(nixosModules): :bulb: add resource on auto-upgrade service

--- nixosModules/system/autoupgrade.nix
+++ nixosModules/system/autoupgrade.nix
@@ -1,8 +1,21 @@
 /**
   # Automatic system upgrades
 
+  To manually start auto-upgrade:
+
+  ```bash
+  sudo systemctl start nixos-upgrade.service
+  ```
+
+  To inspect auto-upgrade results:
+
+  ```bash
+  sudo journalctl --unit nixos-upgrade.service
+  ```
+
   - [Automatic system upgrades @ NixOS Wiki](https://nixos.wiki/wiki/Automatic_system_upgrades).
   - [system.autoUpgrade @ NixOS reference](https://search.nixos.org/options?query=system.autoUpgrade).
+  - [Keeping a NixOS system up-to-date @ Zander Otavka](https://zanderotavka.com/blog/2024/07/08/keeping-a-nixos-system-up-to-date/).
 */
 { config, lib, ... }:
 let
