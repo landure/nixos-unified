47bffeb00ea123d0349cdaed1a6c7f3544b68815
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-12-22 08:45:25 +0100

    feat(nixosConfigurations): :sparkles: add co-bre-p0101 configuration

---
+++ nixosConfigurations/co-bre-p0101/default.nix
@@ -0,0 +1,51 @@
+/**
+  # co-bre-p0101
+
+  NixOS WSL on Windows 11 laptop.
+*/
+{ self, ... }:
+let
+  inherit (self.nixos-unified.lib) mkLinuxSystem;
+
+  mainUser = "pierre-yves";
+  stateVersion = "24.11";
+in
+{
+  endor = mkLinuxSystem { home-manager = true; } {
+    nixpkgs.hostPlatform = "x86_64-linux";
+    imports = [
+      self.nixosModules.default
+      self.nixos-wsl.nixosModules.default
+
+      # Your machine's configuration.nix goes here
+      (
+        { pkgs, ... }:
+        {
+          networking.hostName = "co-bre-p0101";
+          nixos-unified.sshTarget = "${mainUser}@192.168.2.165";
+
+          users.users.${mainUser}.isNormalUser = true;
+
+          system.stateVersion = stateVersion;
+
+          facter.reportPath = ./facter.json;
+
+          biapy.nixos-unified.nixos = {
+            presets.wsl = {
+              enable = true;
+              defaultUser = mainUser;
+            };
+          };
+        }
+      )
+
+      # Setup home-manager in NixOS config
+      {
+        home-manager.users.${mainUser} = {
+          imports = [ self.homeModules.default ];
+          home.stateVersion = stateVersion;
+        };
+      }
+    ];
+  };
+}
